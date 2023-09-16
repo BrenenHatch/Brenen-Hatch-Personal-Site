74bae6da86dfde9b1154a5b8d7476e43b34d18ec
BrenenHatch <brenen.hatch21@gmail.com>
2023-09-16 12:07:46 -0500

    save

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Brenen Hatch
         </a>
       </header>
     </div>
