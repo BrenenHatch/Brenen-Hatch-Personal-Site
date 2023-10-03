4e85100804889562645117abdcf7ff2843f93858
BrenenHatch <brenen.hatch21@gmail.com>
2023-10-02 21:16:15 -0500

    started creating outline for app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Brenen Hatch
-        </a>
+        <h1>Brenen Hatch</h1>
+        <p>Full stack engineer</p>
       </header>
+      <main>
+        <section>
+          <h2>About Me</h2>
+          <p>
+            This is my personal site, it is currently a work in Progress
+          </p>
+        </section>
+        <section>
+          <h2>Portfolio</h2>
+          {/* Add your portfolio projects here */}
+        </section>
+        <section>
+          <h2>Contact</h2>
+          <p>
+            You can reach out to me at: <a href="mailto:brenen.hatch21@gmail.com">brenen.hatch21@gmail.com</a>
+          </p>
+        </section>
+      </main>
+      <footer>
+        <p>&copy; {new Date().getFullYear()} Your Name</p>
+      </footer>
     </div>
   );
 }
 
 export default App;
+
+
